7fd4ed3c6341a4cc8a9a1b625e36447170bba570
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2024-09-10 19:48:31 +0300

    step 5-6

---
+++ src/db/models/contact.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    photo: { type: String },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+const ContactsCollection = model('сontacts', contactSchema);
+export default ContactsCollection;
---
+++ src/services/contacts.js
@@ -0,0 +1,4 @@
+import ContactsCollection from "../db/models/contact.js";
+
+export const getAllContacts = () => ContactsCollection.find();
+export const getContactById = contactId => ContactsCollection.findById(contactId);
